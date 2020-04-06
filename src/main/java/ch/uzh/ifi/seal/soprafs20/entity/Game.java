1837b1f146201a8674ee3fa44cab176465007f61
Tannebaum007 <tim.brunner@uzh.ch>
2020-04-06 13:49:20 +0200

    join/create lobby: implemented game

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity/Game.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/Game.java
@@ -3,17 +3,14 @@ package ch.uzh.ifi.seal.soprafs20.entity;
 import ch.uzh.ifi.seal.soprafs20.boardcomponents.Board;
 import ch.uzh.ifi.seal.soprafs20.boardcomponents.Scoreboard;
 import ch.uzh.ifi.seal.soprafs20.constant.GameStatus;
-import ch.uzh.ifi.seal.soprafs20.constant.UserStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
- * Internal User Representation
- * This class composes the internal representation of the user and defines how the user is stored in the database.
+ * Internal Game Representation
+ * This class composes the internal representation of the game and defines how the game is stored in the database.
  * Every variable will be mapped into a database field with the @Column annotation
  * - nullable = false -> this cannot be left empty
  * - unique = true -> this value must be unique across the database -> composes the primary key
@@ -27,33 +24,26 @@ public class Game implements Serializable {
     @GeneratedValue
     private Long id;
 
-    @Transient
-    private List<Player> players;
+    @Column(nullable = false)
+    private Long ownerId;
 
-
-    @Transient
-    private Scoreboard scoreboard;
+    @Column(nullable = false)
+    private Long chatId;
 
     @Column(nullable = false)
     private GameStatus status;
-
-    @Column(nullable = false)
-    private int ownerid;
-
-    @Column(nullable = false)
-    private int chatid;
 
     @Column
     private String password;
 
     @Transient
+    private List<Player> players;
+
+    @Transient
+    private Scoreboard scoreboard;
+
+    @Transient
     private Board board;
-
-
-
-
-
-
 
     public Long getId() {
         return id;
@@ -63,4 +53,63 @@ public class Game implements Serializable {
         this.id = id;
     }
 
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public void removePlayer(Player player) {
+        players.remove(player);
+    }
+
+    public GameStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(GameStatus status) {
+        this.status = status;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(long id) {
+        this.ownerId = id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long id) {
+        this.chatId = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public Scoreboard getScoreboard() {
+        return scoreboard;
+    }
+
+    public void setScoreboard(Scoreboard scoreboard) {
+        this.scoreboard = scoreboard;
+    }
 }
