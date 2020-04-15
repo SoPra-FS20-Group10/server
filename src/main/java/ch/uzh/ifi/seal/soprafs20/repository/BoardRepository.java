c2b3e7ac108e0f0b91ce58726bafad42a713c02a
shnippi <janjsoef.schnyder@bluewin.ch>
2020-04-15 10:57:56 +0200

    boardrepofix

--- src/main/java/ch/uzh/ifi/seal/soprafs20/repository/BoardRepository.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/repository/BoardRepository.java
@@ -1,5 +1,6 @@
 package ch.uzh.ifi.seal.soprafs20.repository;
 
+import ch.uzh.ifi.seal.soprafs20.entity.Board;
 import ch.uzh.ifi.seal.soprafs20.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface BoardRepository extends JpaRepository<Game, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    Optional<Board> findByIdIs(long id);
 
 }
