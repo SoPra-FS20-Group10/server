ce4f740cef2c91dff725383245d19010db5ade43
shnippi <jan.schnyder@uzh.ch>
2020-04-22 14:59:17 +0200

    baordservice grid

--- src/main/java/ch/uzh/ifi/seal/soprafs20/service/BoardService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/service/BoardService.java
@@ -2,6 +2,7 @@ package ch.uzh.ifi.seal.soprafs20.service;
 
 import ch.uzh.ifi.seal.soprafs20.entity.Board;
 import ch.uzh.ifi.seal.soprafs20.entity.Tile;
+import ch.uzh.ifi.seal.soprafs20.exceptions.NotFoundException;
 import ch.uzh.ifi.seal.soprafs20.repository.BoardRepository;
 import ch.uzh.ifi.seal.soprafs20.repository.GameRepository;
 import ch.uzh.ifi.seal.soprafs20.repository.TileRepository;
@@ -10,17 +11,87 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
-import java.util.List;
+
+import java.util.*;
 
 @Service
 @Transactional
 public class BoardService {
     private final BoardRepository boardRepository;
+    private final TileRepository tileRepository;
 
     @Autowired
-    public BoardService(@Qualifier("boardRepository") BoardRepository boardRepository) {
+    public BoardService(@Qualifier("boardRepository") BoardRepository boardRepository,@Qualifier("tileRepository") TileRepository tileRepository ) {
         this.boardRepository = boardRepository;
+        this.tileRepository = tileRepository;
+    }
+
+    public void creategrid(Board board){
+
+        List<Tile> grid = board.getGrid();
+        grid.clear();
+
+        int[] doubles = {3, 11,16,20,24,28,32,36,38,42,45,48,52,56,59,64,70,76,80,84,88,92,96,98,102,108};
+        int[] triples= {0,7,14,105};
+
+        //create half of board, then flip and append testtest
+
+        for(int i =0; i < 112; i++){
+
+            //check if double field
+
+            if(Arrays.asList(doubles).contains(i)){
+                Optional<Tile> foundTile = tileRepository.findByMultiplierAndStoneSymbol(2,null);
+
+                // check if game exists
+                if (foundTile.isEmpty()) {
+                    throw new NotFoundException("grid cannot be initilazed since the tiles couldnt be found");
+                } else {
+                    grid.add(foundTile.get());
+                }
+            }
+
+            //check if triple tile
+
+            else if(Arrays.asList(triples).contains(i)){
+
+                Optional<Tile> foundTile = tileRepository.findByMultiplierAndStoneSymbol(3,null);
+
+                // check if game exists
+                if (foundTile.isEmpty()) {
+                    throw new NotFoundException("grid cannot be initilazed since the tiles couldnt be found");
+                } else {
+                    grid.add(foundTile.get());
+                }
+
+            }
+
+            //else its single tile
+            else{
+
+
+                Optional<Tile> foundTile = tileRepository.findByMultiplierAndStoneSymbol(1,null);
+
+                // check if game exists
+                if (foundTile.isEmpty()) {
+                    throw new NotFoundException("grid cannot be initilazed since the tiles couldnt be found");
+                } else {
+                    grid.add(foundTile.get());
+                }
+
+            }
+
+        }
+
+        //make a clone and reverse it
+        List<Tile> clone = new ArrayList<Tile>(grid);
+        Collections.reverse(clone);
+        //add the middle star
+        grid.add(tileRepository.findByMultiplierAndStoneSymbol(2,null).get());
+        grid.addAll(clone);
+
+
+        board.setGrid(grid);
     }
 
 
