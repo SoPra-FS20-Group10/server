be18c0f1ad35b593dbcbe2140ce6e37f4e685ae9
shnippi <jan.schnyder@uzh.ch>
2020-05-01 11:39:46 +0200

    integrating tileservice in code

--- src/main/java/ch/uzh/ifi/seal/soprafs20/service/TileService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/service/TileService.java
@@ -1,10 +1,14 @@
 package ch.uzh.ifi.seal.soprafs20.service;
 
+import ch.uzh.ifi.seal.soprafs20.entity.Game;
 import ch.uzh.ifi.seal.soprafs20.entity.Tile;
+import ch.uzh.ifi.seal.soprafs20.exceptions.NotFoundException;
 import ch.uzh.ifi.seal.soprafs20.repository.TileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class TileService {
@@ -39,4 +43,90 @@ public class TileService {
             }
         }
     }
+
+    public void createGrid(Game game){
+        List<Tile> grid = new ArrayList<>();
+
+        Integer[] doubles = {3, 11,16,28,32,36,38,42,45,48,52,56,59,64,70,84,92,96,98,102,108};
+        Integer[] triples= {0,7,14,20,24,76,80,84,88,105};
+        Integer[] doubleWord = {16,28,32,42,48,56,64,70};
+        Integer[] tripleWord = {0,7,14,105};
+
+        String error = "Grid cannot be initialized since the tiles couldn't be found";
+
+        //create half of board, then flip and append testTest
+        for(int i =0; i < 112; i++){
+
+            //check if double field
+            if(Arrays.asList(doubles).contains(i)){
+
+                //check if word or letter
+                Optional<Tile> foundTile;
+                if(Arrays.asList(doubleWord).contains(i)){
+                    foundTile = tileRepository.findByMultiplierAndStoneSymbolAndMultivariant(2, null, "w");
+                } else {
+                    foundTile = tileRepository.findByMultiplierAndStoneSymbolAndMultivariant(2, null, "l");
+                }
+
+                // check if tile exists and add to grid if yes
+                if (foundTile.isEmpty()) {
+                    throw new NotFoundException(error);
+                } else {
+                    grid.add(foundTile.get());
+                }
+            }
+
+            //check if triple tile
+            else if(Arrays.asList(triples).contains(i)){
+                Optional<Tile> foundTile;
+
+                //check if word or letter
+                if(Arrays.asList(tripleWord).contains(i)){
+                    foundTile = tileRepository.findByMultiplierAndStoneSymbolAndMultivariant(3, null, "w");
+                } else {
+                    foundTile = tileRepository.findByMultiplierAndStoneSymbolAndMultivariant(3, null, "l");
+                }
+
+                // check if tile exists and add to grid if yes
+                if (foundTile.isEmpty()) {
+                    throw new NotFoundException(error);
+                } else {
+                    grid.add(foundTile.get());
+                }
+            }
+
+            //else its single tile
+            else {
+                Optional<Tile> foundTile = tileRepository.findByMultiplierAndStoneSymbolAndMultivariant(1,null,"l");
+
+                // check if tile exists
+                if (foundTile.isEmpty()) {
+                    throw new NotFoundException(error);
+                } else {
+                    grid.add(foundTile.get());
+                }
+            }
+        }
+
+        //make a clone and reverse it
+        List<Tile> clone = new ArrayList<>(grid);
+        Collections.reverse(clone);
+
+        //add the middle star
+        Optional<Tile> foundTile = tileRepository.findByMultiplierAndStoneSymbolAndMultivariant(2,null,"w");
+
+        // check if tile exists
+        if (foundTile.isEmpty()) {
+            throw new NotFoundException(error);
+        } else {
+            grid.add(foundTile.get());
+        }
+
+        // fill second half of the grid
+        grid.addAll(clone);
+
+        // add grid to game
+        game.setGrid(grid);
+    }
+
 }
