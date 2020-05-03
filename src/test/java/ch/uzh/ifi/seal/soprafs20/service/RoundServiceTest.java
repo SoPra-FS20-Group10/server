8cdec95d8c9bae2796d44f98c30f1acb1ea7b0ea
Tannebaum007 <tim.brunner@uzh.ch>
2020-05-03 10:26:44 +0200

    FS20G10-216 implemented placeWord test :D

--- src/test/java/ch/uzh/ifi/seal/soprafs20/service/RoundServiceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/service/RoundServiceTest.java
@@ -4,6 +4,7 @@ import ch.uzh.ifi.seal.soprafs20.entity.*;
 import ch.uzh.ifi.seal.soprafs20.repository.GameRepository;
 import ch.uzh.ifi.seal.soprafs20.repository.PlayerRepository;
 import ch.uzh.ifi.seal.soprafs20.repository.StoneRepository;
+import ch.uzh.ifi.seal.soprafs20.repository.TileRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
 
 
 public class RoundServiceTest {
@@ -29,8 +31,15 @@ public class RoundServiceTest {
     @Mock
     private StoneRepository stoneRepository;
 
+    @Mock
+    private TileRepository tileRepository;
+
     @InjectMocks
     private RoundService roundService;
+
+    @InjectMocks
+    private GameService gameService;
+
     private Game testGame;
     private Player testPlayer;
     private Stone stone1, stone2;
@@ -53,34 +62,34 @@ public class RoundServiceTest {
 
         stone1 = new Stone();
         stone1.setId(1L);
-        stone1.setSymbol("a");
+        stone1.setSymbol("g");
         stone1.setValue(3);
 
         stone2 = new Stone();
         stone2.setId(2L);
-        stone2.setSymbol("b");
+        stone2.setSymbol("o");
         stone2.setValue(5);
+
+        Tile tile = new Tile(1, null, "l");
 
         // when -> any object is being save in the gameRepository -> return the dummy testGame
         Mockito.when(gameRepository.save(Mockito.any())).thenReturn(testGame);
+        Mockito.when(tileRepository.findByMultiplierAndStoneSymbolAndMultivariant(Mockito.anyInt(), Mockito.isNull(),
+                Mockito.anyString())).thenReturn(Optional.of(tile));
     }
 
     @Test
     public void test_pointCalculation_no_multiplier() {
         Tile tile1 = new Tile(1, null,"l");
-        tile1.setMultiplier(1);
         tile1.setValue(4);
 
         Tile tile2 = new Tile(1, null,"w");
-        tile2.setMultiplier(1);
         tile2.setValue(1);
 
         Tile tile3 = new Tile(1, null,"l");
-        tile3.setMultiplier(1);
         tile3.setValue(6);
 
         Tile tile4 = new Tile(1, null,"l");
-        tile4.setMultiplier(1);
         tile4.setValue(3);
 
         List<Tile> tiles = new ArrayList<>();
@@ -94,20 +103,16 @@ public class RoundServiceTest {
 
     @Test
     public void test_pointCalculation_doubleLetter() {
-        Tile tile1 = new Tile(1, null,"l");
-        tile1.setMultiplier(2);
+        Tile tile1 = new Tile(2, null,"l");
         tile1.setValue(4);
 
-        Tile tile2 = new Tile(2, null,"w");
-        tile2.setMultiplier(1);
+        Tile tile2 = new Tile(1, null,"w");
         tile2.setValue(1);
 
-        Tile tile3 = new Tile(3, null,"l");
-        tile3.setMultiplier(1);
+        Tile tile3 = new Tile(1, null,"l");
         tile3.setValue(6);
 
-        Tile tile4 = new Tile(1, null,"l");
-        tile4.setMultiplier(2);
+        Tile tile4 = new Tile(2, null,"l");
         tile4.setValue(3);
 
         List<Tile> tiles = new ArrayList<>();
@@ -121,20 +126,16 @@ public class RoundServiceTest {
 
     @Test
     public void test_pointCalculation_tripleLetter() {
-        Tile tile1 = new Tile(1, null,"l");
-        tile1.setMultiplier(3);
+        Tile tile1 = new Tile(3, null,"l");
         tile1.setValue(4);
 
-        Tile tile2 = new Tile(2, null,"w");
-        tile2.setMultiplier(1);
+        Tile tile2 = new Tile(1, null,"w");
         tile2.setValue(1);
 
-        Tile tile3 = new Tile(3, null,"l");
-        tile3.setMultiplier(1);
+        Tile tile3 = new Tile(1, null,"l");
         tile3.setValue(6);
 
-        Tile tile4 = new Tile(1, null,"l");
-        tile4.setMultiplier(3);
+        Tile tile4 = new Tile(3, null,"l");
         tile4.setValue(3);
 
         List<Tile> tiles = new ArrayList<>();
@@ -149,19 +150,15 @@ public class RoundServiceTest {
     @Test
     public void test_pointCalculation_doubleWord() {
         Tile tile1 = new Tile(1, null,"l");
-        tile1.setMultiplier(1);
         tile1.setValue(4);
 
         Tile tile2 = new Tile(2, null,"w");
-        tile2.setMultiplier(2);
         tile2.setValue(2);
 
-        Tile tile3 = new Tile(3, null,"l");
-        tile3.setMultiplier(1);
+        Tile tile3 = new Tile(1, null,"l");
         tile3.setValue(6);
 
-        Tile tile4 = new Tile(1, null,"l");
-        tile4.setMultiplier(3);
+        Tile tile4 = new Tile(3, null,"l");
         tile4.setValue(2);
 
         List<Tile> tiles = new ArrayList<>();
@@ -176,19 +173,15 @@ public class RoundServiceTest {
     @Test
     public void test_pointCalculation_tripleWord() {
         Tile tile1 = new Tile(1, null,"l");
-        tile1.setMultiplier(1);
         tile1.setValue(4);
 
-        Tile tile2 = new Tile(2, null,"w");
-        tile2.setMultiplier(3);
+        Tile tile2 = new Tile(3, null,"w");
         tile2.setValue(2);
 
-        Tile tile3 = new Tile(3, null,"l");
-        tile3.setMultiplier(1);
+        Tile tile3 = new Tile(1, null,"l");
         tile3.setValue(6);
 
-        Tile tile4 = new Tile(1, null,"l");
-        tile4.setMultiplier(3);
+        Tile tile4 = new Tile(3, null,"l");
         tile4.setValue(2);
 
         List<Tile> tiles = new ArrayList<>();
@@ -202,20 +195,16 @@ public class RoundServiceTest {
 
     @Test
     public void test_pointCalculation_everything_mixed() {
-        Tile tile1 = new Tile(1, null,"l");
-        tile1.setMultiplier(2);
+        Tile tile1 = new Tile(2, null,"l");
         tile1.setValue(4);
 
-        Tile tile2 = new Tile(2, null,"w");
-        tile2.setMultiplier(3);
+        Tile tile2 = new Tile(3, null,"w");
         tile2.setValue(2);
 
-        Tile tile3 = new Tile(3, null,"l");
-        tile3.setMultiplier(1);
+        Tile tile3 = new Tile(1, null,"l");
         tile3.setValue(6);
 
-        Tile tile4 = new Tile(1, null,"l");
-        tile4.setMultiplier(3);
+        Tile tile4 = new Tile(3, null,"l");
         tile4.setValue(2);
 
         List<Tile> tiles = new ArrayList<>();
@@ -272,5 +261,61 @@ public class RoundServiceTest {
 
         assertTrue(testPlayer.getBag().contains(stone1));
         assertTrue(testPlayer.getBag().contains(stone2));
+    }
+
+    @Test
+    public void test_placeWord_successful() {
+        // given
+        Tile tile1 = new Tile(3, null, "w");
+        Tile tile2 = new Tile(1, null, "l");
+        Tile tile3 = new Tile(3, "g", "w");
+        Tile tile4 = new Tile(1, "o", "l");
+
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("test");
+
+        Player player = new Player();
+        player.setId(1L);
+        player.setUsername("test");
+        player.setScore(0);
+        player.setUser(user);
+        player.initPlayer();
+        player.addStone(stone1);
+        player.addStone(stone2);
+
+        Game game = new Game();
+        game.setName("test");
+        game.setPassword("test");
+
+        gameService.createGame(game, player);
+        assertNotNull(game, "The created game should not be null");
+
+        List<Long> stoneIds = new ArrayList<>();
+        stoneIds.add(1L);
+        stoneIds.add(2L);
+
+        List<Integer> coordinates = new ArrayList<>();
+        coordinates.add(0);
+        coordinates.add(1);
+
+        // when -> then
+        Mockito.when(stoneRepository.findByIdIs(1)).thenReturn(Optional.ofNullable(stone1));
+        Mockito.when(stoneRepository.findByIdIs(2)).thenReturn(Optional.ofNullable(stone2));
+        Mockito.when(tileRepository.findByMultiplierAndStoneSymbolAndMultivariant(eq(3), Mockito.isNull(),
+                eq("w"))).thenReturn(Optional.of(tile1));
+        Mockito.when(tileRepository.findByMultiplierAndStoneSymbolAndMultivariant(eq(1), Mockito.isNull(),
+                eq("l"))).thenReturn(Optional.of(tile2));
+        Mockito.when(tileRepository.findByMultiplierAndStoneSymbolAndMultivariant(eq(1), eq("g"),
+                eq("l"))).thenReturn(Optional.of(tile3));
+        Mockito.when(tileRepository.findByMultiplierAndStoneSymbolAndMultivariant(eq(1), eq("o"),
+                eq("l"))).thenReturn(Optional.of(tile4));
+
+        // test method
+        roundService.placeWord(game, player, stoneIds, coordinates);
+
+        assertEquals("g", game.getGrid().get(0).getStoneSymbol());
+        assertEquals("o", game.getGrid().get(1).getStoneSymbol());
+        assertTrue(player.getBag().isEmpty());
     }
 }
