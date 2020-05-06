88317566755004d374155e580a83a245cf33b169
shnippi <jan.schnyder@uzh.ch>
2020-05-06 16:45:22 +0200

    added greeting class FS20G10-134

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/testclasses/Greeting.java
@@ -0,0 +1,16 @@
+package ch.uzh.ifi.seal.soprafs20.testclasses;
+
+public class Greeting {
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
