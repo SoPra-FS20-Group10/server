1696fe4a7e322fa197561330099d549e5e1ebac9
shnippi <jan.schnyder@uzh.ch>
2020-05-24 12:51:12 +0200

    stonedto test FS20G10-285

--- src/test/java/ch/uzh/ifi/seal/soprafs20/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/rest/mapper/DTOMapperTest.java
@@ -4,11 +4,9 @@ import ch.uzh.ifi.seal.soprafs20.constant.PlayerStatus;
 import ch.uzh.ifi.seal.soprafs20.constant.UserStatus;
 import ch.uzh.ifi.seal.soprafs20.entity.Game;
 import ch.uzh.ifi.seal.soprafs20.entity.Player;
+import ch.uzh.ifi.seal.soprafs20.entity.Stone;
 import ch.uzh.ifi.seal.soprafs20.entity.User;
-import ch.uzh.ifi.seal.soprafs20.rest.dto.GamePostDTO;
-import ch.uzh.ifi.seal.soprafs20.rest.dto.PlayerGetDTO;
-import ch.uzh.ifi.seal.soprafs20.rest.dto.UserGetDTO;
-import ch.uzh.ifi.seal.soprafs20.rest.dto.UserPostDTO;
+import ch.uzh.ifi.seal.soprafs20.rest.dto.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -74,7 +72,12 @@ class DTOMapperTest {
         player.setStatus(PlayerStatus.NOT_READY);
         player.setScore(300);
 
+        // MAP
+
         PlayerGetDTO playerGetDTO = DTOMapper.INSTANCE.convertEntityToPlayerGetDTO(player);
+
+        // check content
+
 
         assertEquals(player.getScore(),playerGetDTO.getScore());
         assertEquals(player.getId(),playerGetDTO.getId());
@@ -82,5 +85,26 @@ class DTOMapperTest {
         assertEquals(player.getStatus(),playerGetDTO.getStatus());
         assertEquals(player.getScore(),playerGetDTO.getScore());
 
+    }
+
+    @Test
+    public void test_convertEntityToStoneGetDTO(){
+        //create Stone
+        Stone stone = new Stone();
+        stone.setId(1L);
+        stone.setSymbol("a");
+        stone.setValue(2);
+
+        // MAP
+
+
+        StoneGetDTO stoneGetDTO = DTOMapper.INSTANCE.convertEntityToStoneGetDTO(stone);
+
+        // check content
+
+
+        assertEquals(stone.getId(),stoneGetDTO.getId());
+        assertEquals(stone.getSymbol(),stoneGetDTO.getSymbol());
+        assertEquals(stone.getValue(),stoneGetDTO.getValue());
     }
 }
