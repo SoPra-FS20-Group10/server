0ea7c1f721aef41b741a8a9f237fb344190add3a
shnippi <jan.schnyder@uzh.ch>
2020-05-24 12:23:49 +0200

    added comments for stati and wordlist

--- src/main/java/ch/uzh/ifi/seal/soprafs20/constant/GameStatus.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/constant/GameStatus.java
@@ -1,5 +1,8 @@
 package ch.uzh.ifi.seal.soprafs20.constant;
 
+
+// status indication for the game
+
 public enum GameStatus {
     WAITING, RUNNING, ENDED;
 }
--- src/main/java/ch/uzh/ifi/seal/soprafs20/constant/PlayerStatus.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/constant/PlayerStatus.java
@@ -1,5 +1,7 @@
 package ch.uzh.ifi.seal.soprafs20.constant;
 
+// status indication for the player
+
 public enum PlayerStatus {
     READY, NOT_READY;
 }
